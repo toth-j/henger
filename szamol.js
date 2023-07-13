9e8624147bd76cff1142ff2e39973016165ab09e
toth-j <tothj123@hotmail.com>
2023-07-13 11:35:05 +0200

    Negatív számok esetén hibaüzenet

--- szamol.js
+++ szamol.js
@@ -2,11 +2,16 @@ const szamol = () => {
     let r = +document.getElementById("sugar").value;
     let m = +document.getElementById("magas").value;
 
-    let A = 2 * r * Math.PI * (r + m);
-    let V = r * r * Math.PI * m;
-
-    document.getElementById("felszin").innerHTML  = "Felszín:  " + A.toFixed(3);
-    document.getElementById("terfogat").innerHTML = "Térfogat: " + V.toFixed(3);
+    if (r > 0 && m > 0) {
+        let A = 2 * r * Math.PI * (r + m);
+        let V = r * r * Math.PI * m;
+        document.getElementById("felszin").innerHTML = "Felszín:  " + A.toFixed(3);
+        document.getElementById("terfogat").innerHTML = "Térfogat: " + V.toFixed(3);
+    }
+    else {
+        document.getElementById("felszin").innerHTML = "Hibás adatok!";
+        document.getElementById("terfogat").innerHTML = "";
+    }
 }
 
 document.getElementById("gomb").onclick = szamol;
